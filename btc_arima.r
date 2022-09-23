b1538d9bd51acc5ae49487529e8edbc9436194ee
evanchen1233 <101177476+evanchen1233@users.noreply.github.com>
2022-09-23 16:46:29 +1000

    update

--- btc_arima.r
+++ btc_arima.r
@@ -13,18 +13,22 @@ r <- coin_history(coin_id = "bitcoin", vs_currency = "usd", days = "max")
 var <- c("timestamp", "price")
 df <- r[var]
 df <- df[c(1:nrow(df) - 1),]
-df <- ts(df["price"], start = c(2013,4,28), frequency = 365)
+tail(df,10)
+date <- df$timestamp
+start_date <- date[1]
+dayOfYear <- as.numeric(format(as.Date(start_date),"%j"))
+year <- as.numeric(format(as.Date(start_date),"%Y"))
+df <- ts(df$price, start = c(year, dayOfYear), frequency = 365)
+tail(df,10)
 
-n = nrow(df)
-n1 = nrow(df) - 6
-n2 = nrow(df) - 7
+n = length(df)
+n1 = n - 6
+n2 = n - 7
 
+df_train = df[c(1:n2)]
+df_test = df[c(n1:n)]
 
-df_train = ts(df[c(4:n2),])
-df_test = ts(df[c(n1:n),])
-df_full = ts(df[c(4:n),])
-
-adf.test(df_full)
+adf.test(df)
 
 model <- auto.arima(df_train)
 fcast <- forecast(model, h = 7, level = 95)
@@ -32,8 +36,10 @@ fcast <- forecast(model, h = 7, level = 95)
 mape <- mape(df_test,as.numeric(fcast$mean))
 mape*100
 
-result <- cbind(df_test,as.numeric(fcast$mean))
-colnames(result) <- c("Actual","auto.arima")
+result <- cbind(as.numeric(fcast$mean),df_test)
+colnames(result) <- c("auto.arima","Actual")
 result
 
 autoplot(fcast) + (labs(y = "Price", x = "Days"))
+
+
